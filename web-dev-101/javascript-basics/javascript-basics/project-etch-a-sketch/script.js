e992d621484ac62cd9238d810e46984fc915e003
Chris Vaughan <vaughandroid@gmail.com>
2020-07-04 12:14:41 +0100

    Make grid squares visible and remove placeholder text.

--- web-dev-101/javascript-basics/javascript-basics/project-etch-a-sketch/script.js
+++ web-dev-101/javascript-basics/javascript-basics/project-etch-a-sketch/script.js
@@ -5,19 +5,18 @@ window.onload = () => {
 function createGrid() {
   const rows = 16;
   const columns = rows;
+  const squareSize = '30px';
 
   container = document.querySelector('#grid-container');
-  container.style.display = 'grid';
-  let columnsStyle = '';
-  for (let i = 0; i < columns; i++) {
-    columnsStyle += 'auto '
-  }
-  container.style.gridTemplateColumns = columnsStyle;
-
+  container.style.display = 'inline-grid';
+  container.style.gridTemplateColumns = `repeat(${columns}, ${squareSize})`;
+  container.style.gridTemplateRows = `repeat(${rows}, ${squareSize})`;
+  container.style.border = '2px solid black';
+  
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
       let squareDiv = document.createElement('div');
-      squareDiv.textContent = `(${i}, ${j})`
+      squareDiv.style.border = '0.5px solid black';
       container.appendChild(squareDiv);
     }
   }
