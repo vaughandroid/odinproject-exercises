c902ccf67b4a26940ae2ef459566666ea032aea2
Chris Vaughan <vaughandroid@gmail.com>
2020-07-05 08:14:10 +0100

    Implement clearing of grid and prompting for a new number of rows & columns.

--- web-dev-101/javascript-basics/javascript-basics/project-etch-a-sketch/script.js
+++ web-dev-101/javascript-basics/javascript-basics/project-etch-a-sketch/script.js
@@ -1,25 +1,40 @@
 window.onload = () => {
-  createGrid();
+  document.querySelector('#clear-button').addEventListener('click', () => {
+    const input = prompt('How many rows and columns would you like? (1-100)');
+    let rows = Number.parseInt(input);
+    if (Number.isNaN(rows)
+      || rows <= 0
+      || rows > 100) {
+      console.log('Using default of 16 rows.');
+      rows = 16;
+    }
+
+    createGrid(rows);
+  });
+
+  createGrid(16);
 }
 
-function createGrid() {
+function createGrid(rows) {
   const gridSize = 500;
-  const rows = 16;
   const columns = rows;
   const squareSize = `${gridSize / rows}px`;
 
   grid = document.querySelector('#grid');
-  grid.style.display = 'grid'; 
+
+  grid.innerHTML = '';
+
+  grid.style.display = 'grid';
   grid.style.margin = '0 auto';
   grid.style.border = '2px solid black';
   grid.style.width = `${gridSize + 4}px`;
   grid.style.gridTemplateColumns = `repeat(${columns}, ${squareSize})`;
   grid.style.gridTemplateRows = `repeat(${rows}, ${squareSize})`;
-  
+
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
       let squareDiv = document.createElement('div');
-        squareDiv.addEventListener('mouseover', () => { 
+      squareDiv.addEventListener('mouseover', () => {
         squareDiv.style.background = 'black';
       })
       grid.appendChild(squareDiv);
