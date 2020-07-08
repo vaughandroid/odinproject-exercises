7087c583fb52cda13604652dfa93c2275a308802
Chris Vaughan <vaughandroid@gmail.com>
2020-07-08 18:45:56 +0100

    Input numbers.

--- web-dev-101/javascript-basics/javascript-basics/project-calculator/script.js
+++ web-dev-101/javascript-basics/javascript-basics/project-calculator/script.js
@@ -1,3 +1,39 @@
+window.onload = () => {
+  document.querySelectorAll('.number').forEach((btn) => {
+    btn.addEventListener('click', () => {
+      numberPressed(+btn.textContent);
+    });
+  });
+
+  setCurrentValue(0);
+}
+
+function numberPressed(n) {
+  if (currentValue === 0) {
+    setCurrentValue(n);
+  } else {
+    setCurrentValue(currentValue + '' + n);
+  }
+}
+
+function setCurrentValue(v) {
+  document.querySelector('#display').textContent = v;
+  currentValue = v;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -12,17 +48,4 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
-}
-
-function operate(operator, a, b) {
-  switch(operator) {
-    case "+":
-      return add(a, b);
-    case "-":
-      return subtract(a, b);
-    case "x":
-      return multiply(a, b);
-    case "/":
-      return divide(a, b);
-  }
 }
